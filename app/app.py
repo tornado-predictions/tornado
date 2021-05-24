b399685d164f0ebf56a9d2961590803b01af2255
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-05-23 22:11:12 -0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -18,8 +18,10 @@ app = Flask(__name__)
 # Data Setup
 #################################################
 
+# From jupyter notebook file
 model = pickle.load(open('data/model.pkl', 'rb'))
 
+# From jupyter notebook file
 tornado_df = pd.read_csv('data/cleaned.csv')
 
 #################################################
@@ -32,6 +34,24 @@ def home():
     print(model)
     print(tornado_df)
     return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    if request.method == "POST":
+        len = float(request.form["len"])
+        wid = float(request.form["wid"])
+        fat = float(request.form["fat"])
+        slat = float(request.form["slat"])
+        slon = float(request.form["slon"])
+
+        data = [[fat, len, wid, slat, slon]]
+        df_user = pd.DataFrame(data, columns=["fat", "len", "wid", "slat", "slon"])
+        print("User input", df_user)
+        features = tornado_df
+        print("Data References", features)
+
+        return render_template("results.html", classify="working")
 
 # To run application
 if __name__ == '__main__':
