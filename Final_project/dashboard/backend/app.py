07c63ecbee32e2b174d5cc178bd8fd68db672b34
ajinkyabhonsle <31680747+ajinkyabhonsle@users.noreply.github.com>
2021-05-25 19:45:43 -0700

    Add files via upload

--- Final_project/dashboard/backend/app.py
+++ Final_project/dashboard/backend/app.py
@@ -51,42 +51,42 @@ tornado_df = pd.read_csv('data/cleaned.csv')
     # tornado_df = pd.read_csv('data/cleaned.csv')
 
 # loads data from file into mongo db and returns geojson
-    @app.route("/api/mongo")
-    def mongo_data():
-        db = client.geoDB 
-        collection = db.geojson
+@app.route("/api/mongo")
+def mongo_data():
+    db = client.geoDB 
+    collection = db.geojson
 
-        features = pd.read_json('data/data.json')["features"].to_list()
+    features = pd.read_json('data/data.json')["features"].to_list()
 
-        for feature in features:
-            collection.insert_one(feature)
+    for feature in features:
+        collection.insert_one(feature)
 
-        data = db.geojson.find()
-        geo = {
-        "type":"FeatureCollection",
-            "features": []
-        }
-        for d in data:
-            geo["features"].append({
-                "type": d["type"],
-                "geometry": d["geometry"],
-                "properties": d["properties"]
-            })
-        return geo
+    data = db.geojson.find()
+    geo = {
+    "type":"FeatureCollection",
+        "features": []
+    }
+    for d in data:
+        geo["features"].append({
+            "type": d["type"],
+            "geometry": d["geometry"],
+            "properties": d["properties"]
+        })
+    return geo
 
     # reads data from csv and returns top 10 states by causalty
-    @app.route("/api/top10")
-    def top10():
-        top10 = {}
-        top10 = data_cleaning.top10()
-        return top10
+@app.route("/api/top10")
+def top10():
+    top10 = {}
+    top10 = data_cleaning.top10()
+    return top10
 
     # reads data from csv and returns month_year totals for financial loss
-    @app.route("/api/date_loss")
-    def date_loss():
-        date_loss = {}
-        date_loss = data_cleaning.date_loss()
-        return date_loss
+@app.route("/api/date_loss")
+def date_loss():
+    date_loss = {}
+    date_loss = data_cleaning.date_loss()
+    return date_loss
 
     # Render html template
     # @app.route("/")
@@ -96,58 +96,61 @@ tornado_df = pd.read_csv('data/cleaned.csv')
     #     return render_template("index.html")
 
     # Get user input and predict tornado category
-    @app.route("/predict", methods=["POST"])
-    def predict():
+@app.route("/predict", methods=["POST"])
+def predict():
 
-        # convert user input from template.html to float and save as variables
-        if request.method == "POST":
-            leng = float(request.form["leng"])
-            wid = float(request.form["wid"])
-            fat = float(request.form["fat"])
-            place = request.form["place"]
+    # convert user input from template.html to float and save as variables
+    if request.method == "POST":
+        leng = float(request.form["leng"])
+        wid = float(request.form["wid"])
+        fat = float(request.form["fat"])
+        place = request.form["place"]
 
-            # convert place to lat and long
-            locator = Nominatim(user_agent="myGeocoder")
-            location = locator.geocode(place)
+        # convert place to lat and long
+        locator = Nominatim(user_agent="myGeocoder")
+        location = locator.geocode(place)
 
-            slat = location.latitude
-            slon = location.longitude
+        slat = location.latitude
+        slon = location.longitude
 
-            print("Latitude", slat)
-            print("Longitude", slon)
+        print("Latitude", slat)
+        print("Longitude", slon)
 
-            # slat = float(request.form["slat"])
-            # slon = float(request.form["slon"])
+        # slat = float(request.form["slat"])
+        # slon = float(request.form["slon"])
 
-            # store user inputs as dataframe user_df
-            data = [[fat, leng, wid, slat, slon]]
-            user_df = pd.DataFrame(data, columns=["fat", "len", "wid", "slat", "slon"])
-            # store dataframe used for model as features
-            features = tornado_df
-            # append user data to tornado data
-            complete = features.append(user_df)
-            # set up scaler and apply scaling
-            scaler = MinMaxScaler()
-            scaled_df = scaler.fit_transform(complete)
-            # Category prediction of user input from model
-            output = model.predict([list(scaled_df[len(scaled_df)-1])])
+        # store user inputs as dataframe user_df
+        data = [[fat, leng, wid, slat, slon]]
+        user_df = pd.DataFrame(data, columns=["fat", "len", "wid", "slat", "slon"])
+        # store dataframe used for model as features
+        features = tornado_df
+        # append user data to tornado data
+        complete = features.append(user_df)
+        # set up scaler and apply scaling
+        scaler = MinMaxScaler()
+        scaled_df = scaler.fit_transform(complete)
+        # Category prediction of user input from model
+        output = model.predict([list(scaled_df[len(scaled_df)-1])])
 
-            # Rename output to user friendly text
-            category = ""
-            if(output[0] == 0):
-                category = "EF 0 - Damage Light"
-            elif(output[0] == 1):
-                category = "EF 1 - Damage Moderate"
-            elif(output[0] == 2):
-                category = "EF 2 - Damage Considerable"
-            elif(output[0] == 3):
-                category = "EF 3 - Damage Severe"
-            elif(output[0] == 4):
-                category = "EF 4 - Damage Devastating"
-            elif(output[0] == 5):
-                category = "EF 5 - Damage Incredible"
-            
-            return render_template("results.html", classify=category)
+        # Rename output to user friendly text
+        category = ""
+        if(output[0] == 0):
+            category = "EF 0 - Damage Light"
+        elif(output[0] == 1):
+            category = "EF 1 - Damage Moderate"
+        elif(output[0] == 2):
+            category = "EF 2 - Damage Considerable"
+        elif(output[0] == 3):
+            category = "EF 3 - Damage Severe"
+        elif(output[0] == 4):
+            category = "EF 4 - Damage Devastating"
+        elif(output[0] == 5):
+            category = "EF 5 - Damage Incredible"
+
+        print(category)
+        
+        return { "classify": category }
+        #render_template("results.html", classify=category)
 
 
 # To run applicaton
